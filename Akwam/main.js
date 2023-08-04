0cf9a0e50fbd8c30621c485d9e9dc30262118e17
AhmadTech <90527815+AhmadWebTech@users.noreply.github.com>
2023-08-04 23:46:48 +0200

    Update main.js

--- Akwam/main.js
+++ Akwam/main.js
@@ -1,1 +1,10 @@
-
+const linkShowElements = document.querySelectorAll(".link-show");
+const linkDownloadElements = document.querySelectorAll(".link-download");
+const currentMovieId = location.href.split("/")[4];
+linkShowElements.forEach((linkShowElement, index) => {
+  const movieId = linkShowElement.href.split("/").pop();
+  const movieId2 = (movieId === currentMovieId) ? location.href.split("/")[5] : currentMovieId;
+  const baseUrl = location.origin;
+  linkShowElement.href = `${baseUrl}/watch/${movieId}/${movieId2}`;
+  linkDownloadElements[index].href = `${baseUrl}/download/${movieId}/${movieId2}`;
+});
